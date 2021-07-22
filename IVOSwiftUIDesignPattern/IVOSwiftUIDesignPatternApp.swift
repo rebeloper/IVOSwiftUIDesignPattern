4e23ad4878ee3937751278252ce9bf6e4126e285
Alex Nagy <therebeloper@gmail.com>
2021-07-22 11:28:41 +0300

    Initial Commit

---
+++ IVOSwiftUIDesignPattern/IVOSwiftUIDesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  IVOSwiftUIDesignPatternApp.swift
+//  IVOSwiftUIDesignPattern
+//
+//  Created by Alex Nagy on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct IVOSwiftUIDesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
